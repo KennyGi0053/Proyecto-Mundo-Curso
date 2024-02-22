f656fc2ed83663c5ebee0d62748375fd5edc3ebf
KGabriel Perez <121875673+KennyGi0053@users.noreply.github.com>
2024-02-22 19:23:35 -0300

    navbar iconos agregados

--- Mundo Curso Front/src/components/Navbar/Navbar.jsx
+++ Mundo Curso Front/src/components/Navbar/Navbar.jsx
@@ -3,6 +3,8 @@ import styles from './Navbar.module.css'
 import logo from '../../assets/Logo mi mundo Curso.png'
 import { AiOutlineHome } from "react-icons/ai";
 import interrogation from '../../assets/interrogation.png'
+import home from '../../assets/home.png'
+import dollar from '../../assets/dollar.png'
 const Navbar = () => {
 
 
@@ -11,17 +13,16 @@ const Navbar = () => {
         <img className={styles.logo} src={logo} alt='logo' />
       
         <section className={styles.HomeContainer} >
+        <img className={styles.home} src={home} alt='icono home'></img>
         <Link to= '/' className={styles.link}> 
-        <h4 className={styles.iconHome}> <AiOutlineHome /> Inicio </h4>
+        <h4 className={styles.iconHome}> Inicio </h4>
         </Link>
         <img  className={styles.interrogation} src={interrogation} alt='Preguntas frecuentes' />
         <Link className={styles.link} to='Preguntas'>
         <h4 className={styles.frecuente}>Preguntas frecuentes</h4>
         </Link>
-
-        
-
-       <h4 className={styles.p}>Pago seguro</h4>
+        <img className={styles.dollar} src={dollar} alt='simbolo de dolar' ></img>
+        <h4 className={styles.p}>Pago seguro</h4>
         </section >
 
         {/* <img className={styles.garantia} src={Garantia} alt='garantia' /> */}
