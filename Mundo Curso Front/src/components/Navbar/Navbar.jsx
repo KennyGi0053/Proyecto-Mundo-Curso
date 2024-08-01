40c406d4fc6bb056887bb6052a86d970d94c7d12
Kenny Gabriel Perez Rosales <121875673+KennyGi0053@users.noreply.github.com>
2024-08-01 11:19:34 -0400

    cambios en componentes de home

--- Mundo Curso Front/src/components/Navbar/Navbar.jsx
+++ Mundo Curso Front/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import { NavLink } from "react-router-dom"
 import style from './Navbar.module.css'
-import logo from '../../assets/logo mi mundo curso.png'
+import logo from '../../assets/Logo Mundo Curso.png'
 import { useState } from "react";
 const Navbar = () => {
   const [showNav, setShowNav] = useState(false); // Agrega el estado para mostrar/ocultar la barra de navegación
